4012291e8e1df2696eacb35974abae6ea9038848
johnnybee4e <jon.rosado42@gmail.com>
2018-09-06 16:28:45 -0400

    feat(add): sections scrolled to when clicked in navbar

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
-
+import Scrollspy from 'react-scrollspy';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 export default class Header extends Component {
   render() {
+    const offset = document.documentElement.clientHeight * 0.1;
+
+    window.addEventListener('scroll', function(evt) {
+      if (this.window.scrollY > offset) {
+        let appHeader = document.getElementById('nav-links');
+        appHeader.classList.add('newfixed');
+      } else if (this.window.scrollY < offset) {
+        let appHeader = document.getElementById('nav-links');
+        appHeader.classList.remove('newfixed');
+      }
+    });
     return (
-      <div className="navbar">
-        <header className="App-header">
-        <button className="Header-button"><a href="#About-me">Who am I?</a></button>
-        <button className="Header-button"><a href="#Projects">Things I've Built</a></button>
-        <button className="Header-button"><a href="#Experience">Places I've Been</a></button>
-        </header>
-      </div>
+      <header className="navbar">
+        <div id="nav-links">
+          <Scrollspy
+            className="App-header"
+            componentTag={'nav'}
+            currentClassName={'inView'}
+            offset={-offset}
+            items={['About-me', 'Projects', 'Experience']}
+          >
+            <button className="Header-button">
+              <AnchorLink href="#About-me" offset={offset}>
+                Who am I?
+              </AnchorLink>
+            </button>
+            <button className="Header-button">
+              <AnchorLink href="#Projects" offset={offset}>
+                Things I've Built
+              </AnchorLink>
+            </button>
+            <button className="Header-button">
+              <AnchorLink href="#Experience" offset={offset}>
+                Places I've Been
+              </AnchorLink>
+            </button>
+          </Scrollspy>
+        </div>
+      </header>
     );
   }
 }
