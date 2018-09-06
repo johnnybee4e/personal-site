3fe6abb0c34f2c79026db0ce6c6b8db25a4481ea
johnnybee4e <jon.rosado42@gmail.com>
2018-09-06 18:27:26 -0400

    added work experience for FSA Fellowship. Minor styling as well

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
 
-
 export default class Experience extends Component {
   render() {
     return (
-      <div className="Info-card">
-      <h2 id="Experience" className="Body-Title">Experience</h2>
-      <h3 className="Company-name">Fullstack Academy<br/> Teaching Fellow</h3>
-      <p>Af meditation marfa copper mug butcher affogato YOLO chia pitchfork schlitz vice craft beer austin polaroid. You probably haven't heard of them yuccie godard hoodie 8-bit farm-to-table. Actually truffaut flexitarian austin. Truffaut banjo humblebrag jean shorts forage bicycle rights four loko kombucha kickstarter chambray small batch taiyaki. Kickstarter copper mug chambray four dollar toast, viral austin plaid jianbing man braid. Bespoke XOXO ennui williamsburg coloring book sartorial taiyaki.</p>
-      <h3 className="Company-name">MixyPaws<br/>Training Manager</h3>
-      <p>Af meditation marfa copper mug butcher affogato YOLO chia pitchfork schlitz vice craft beer austin polaroid. You probably haven't heard of them yuccie godard hoodie 8-bit farm-to-table. Actually truffaut flexitarian austin. Truffaut banjo humblebrag jean shorts forage bicycle rights four loko kombucha kickstarter chambray small batch taiyaki. Kickstarter copper mug chambray four dollar toast, viral austin plaid jianbing man braid. Bespoke XOXO ennui williamsburg coloring book sartorial taiyaki.</p>
+      <div id="Exp">
+        <div className="Info-card">
+          <h2 id="Experience" className="Body-Title">
+            Experience
+          </h2>
+          <h3 className="Company-name">
+            Fullstack Academy
+            <br /> Teaching Fellow
+          </h3>
+          <p>July 2018 - October 2018</p>
+          <ul className="Exp-desc-list">
+            <li className="Exp-desc-item">
+              Assisted in teaching a cohort of 28 students. Covered concepts
+              from debugging, writing clean code, and alogrithms to
+              understanding technical concepts, and application architecture.
+            </li>
+            <li className="Exp-desc-item">
+              Led multiple lectures on algorithm problems across topics such as
+              big O, optimization, data structures, and functional programming.
+            </li>
+            <li className="Exp-desc-item">
+              Acted as project manager for two project sprints, leading daily
+              standups and holding code reviews
+            </li>
+          </ul>
+          <h3 className="Company-name">
+            MixyPaws
+            <br />
+            Training Manager
+          </h3>
+          <ul className="Exp-desc-list" />
+        </div>
       </div>
     );
   }
